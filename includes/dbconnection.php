02d5a30be39ee368c2319215f4efab2092e4a7d5
Dravya Gohil <57190083+dravya08@users.noreply.github.com>
2021-04-28 23:46:23 +0530

    Update dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "detsdb");
+$con=mysqli_connect("remotemysql.com", "wySv0uNgK2", "6d8bQfn4nK", "wySv0uNgK2");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
