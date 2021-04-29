114925d929d5a819443354da02ce4661ca587ab7
Dravya Gohil <57190083+dravya08@users.noreply.github.com>
2021-04-29 13:03:59 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,3 @@
 <div class="col-sm-12">
-				<p class="back-link">Daily Expense Tracker <a href="https://about.me/ganeshkavhar" target="_blank">ganesh k</a></p>
+				<p class="back-link">Daily Expense Tracker</p>
 			</div>
